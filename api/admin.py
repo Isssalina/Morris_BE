52eb099cb595bee30f8551ac463c3fd77232c179
Zpsun <zhang4430@uwlax.edu>
2022-02-22 13:20:01 +0800

    update

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
 from .models import Caretaker, Healthcareprofessional, Requests, Roles, Securityquestions, Users
 
-admin.site.register(Caretaker)
+
+class CaretakerAdmin(admin.ModelAdmin):
+    list_display = ['takerID', 'firstName', 'lastName', 'phoneNumber', 'postalAddress', 'email', 'enroll', 'userID']
+
+
+class SecurityquestionsAdmin(admin.ModelAdmin):
+    list_display = ['question']
+
+
+class RolesAdmin(admin.ModelAdmin):
+    list_display = ['roleName']
+
+
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ['userID', 'firstName', 'lastName', 'phoneNumber', 'postalAddress', 'email']
+
+
+admin.site.register(Caretaker, CaretakerAdmin)
 admin.site.register(Healthcareprofessional)
 admin.site.register(Requests)
-admin.site.register(Roles)
-admin.site.register(Securityquestions)
-admin.site.register(Users)
+admin.site.register(Roles, RolesAdmin)
+admin.site.register(Securityquestions, SecurityquestionsAdmin)
+admin.site.register(Users, UsersAdmin)
